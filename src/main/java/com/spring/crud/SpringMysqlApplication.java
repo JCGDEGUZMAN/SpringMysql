33b8c676891f082c814e99606a9be4e84b668e35
JCDEGUZMAN <julius.d@cloudwalkdigital.com>
2020-01-27 16:32:21 +0800

    enable cors

--- src/main/java/com/spring/crud/SpringMysqlApplication.java
+++ src/main/java/com/spring/crud/SpringMysqlApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringMysqlApplication extends SpringBootServletInitializer{
@@ -12,9 +16,19 @@ public class SpringMysqlApplication extends SpringBootServletInitializer{
 		SpringApplication.run(SpringMysqlApplication.class, args);
 	}
 	
-	   @Override
-	   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		   return builder.sources(SpringMysqlApplication.class);
-	   }
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	   return builder.sources(SpringMysqlApplication.class);
+   }
+   
+   @Configuration
+   @EnableWebMvc
+   public class WebConfig implements WebMvcConfigurer {
+    
+       @Override
+       public void addCorsMappings(CorsRegistry registry) {
+           registry.addMapping("/**");
+       }
+   }
 
 }
